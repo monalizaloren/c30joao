34549cb3f760c0a408d1652a79d4fe1bf051e9ea
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-21 08:30:03 -0300

    Add files via upload

---
+++ link.js
@@ -0,0 +1,15 @@
+class Link{
+  constructor(bodyA,bodyB)
+  {
+    var lastlink = bodyA.body.bodies.length-2;
+   
+      World.add(engine.world,this.link);
+  } 
+
+  detach()
+  {
+    
+   
+  }
+}
+
